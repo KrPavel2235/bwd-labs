fb1a10f5e06d8bee71b5b0439be3cbdaade44c2e
KrPavel2235 <dcsuper1337@gmail.com>
2024-11-21 00:38:12 +0400

    fix: {LAB4-3}{9} внесены изменения в код для корректного подключения css и js файлов

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,33 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', // Точка входа для сборки проекта
+  entry: './src/джава.js', // Точка входа для сборки проекта
 
   output: {
     filename: 'bundle.js', // Имя выходного файла сборки
     path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
+    clean: true, // Очистка папки dist перед сборкой
   },
 
   module: {
     rules: [
-	{
-        test: /\.js$/,           // Правило для всех файлов с расширением .js
-        exclude: /node_modules/, // Исключаем папку node_modules
-        use: {
-          loader: 'babel-loader',  // Используем babel-loader для обработки JS
-          options: {
-            presets: ['@babel/preset-env'],  // Пресет для компиляции современных JS функций в старые
-          },
-        },
-      },
       {
         test: /\.css$/, // Регулярное выражение для обработки файлов с расширением .css
         use: ['style-loader', 'css-loader'], // Загрузчики для обработки CSS-файлов
       },
-	 
-	  {
+      {
         test: /\.(png|jpe?g|gif|svg)$/i, // Поддержка форматов изображений
         type: 'asset/resource', // Автоматически копирует изображения в `dist`
+      },
+      {
+        test: /\.js$/, // Для обработки JS
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'], // Настройка для совместимости с разными браузерами
+          },
+        },
       },
     ],
   },
@@ -37,7 +37,7 @@ module.exports = {
   plugins: [
     // Плагин для главной страницы
     new HtmlWebpackPlugin({
-      template: './src/index.html', 
+      template: './src/index.html',
       inject: true,
       chunks: ['index'], // Использовать только основной chunk
       filename: 'index.html', // Выходной файл для этой страницы
@@ -45,7 +45,7 @@ module.exports = {
 
     // Плагин для страницы проекта
     new HtmlWebpackPlugin({
-      template: './src/projects.html', 
+      template: './src/projects.html',
       inject: true,
       chunks: ['index'],
       filename: 'projects.html',
@@ -53,7 +53,7 @@ module.exports = {
 
     // Плагин для страницы задач
     new HtmlWebpackPlugin({
-      template: './src/разметка.html', 
+      template: './src/разметка.html',
       inject: true,
       chunks: ['index'],
       filename: 'разметка.html',
@@ -66,6 +66,8 @@ module.exports = {
       chunks: ['index'],
       filename: 'about.html',
     }),
+
+    // Копирование изображений в dist
     new CopyWebpackPlugin({
       patterns: [
         { from: path.resolve(__dirname, 'src/img'), to: 'img' },
